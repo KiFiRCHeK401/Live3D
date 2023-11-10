2a6fbe345e949f5eecc862f0cfc900364839d218
KiFiRCHeK401 <artur-karel@yandex.ru>
2023-11-10 22:14:32 +0400

    v0.1

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject Prefub;
+    public int n = 10;
+    public List<GameObject> particles = new List<GameObject>();
+    public static int count = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    private void SpawnParticle()
+    {
+
+    }
+}
---
+++ Assets/Scripts/TheParticle.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+
+public class TheParticle : MonoBehaviour
+{
+
+    //GameObject obj;
+
+    
+    //public TheParticle(int type,Color color)
+    //{
+    //    this.type = type;
+    //    this.color = color;
+
+    //}
+
+
+    //public void testMethod()
+    //{
+    //    Console.WriteLine("метод вызван");
+    //}
+
+}
