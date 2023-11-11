3e8d722e81b76d03314cf6974ce1e1e1f23e834b
KiFiRCHeK401 <artur-karel@yandex.ru>
2023-11-11 11:15:54 +0400

    v0.1..0.1
    can create and delete particles without parameters

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,33 +1,57 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Numerics;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+    public double deltaTime = 0;
     public GameObject Prefub;
-    public int n = 10;
-    public List<GameObject> particles = new List<GameObject>();
-    public static int count = 0;
+    public HashSet<GameObject> particles = new HashSet<GameObject>();
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
 
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        if(deltaTime < 0.1f ) { deltaTime += Time.deltaTime; return; }
+        if (Input.anyKey) { deltaTime = 0; }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            particles.Add(Instantiate(Prefub, transform.position, UnityEngine.Quaternion.identity));
+        }
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            Destroy(particles.Last());
+            particles.Remove(particles.Last());
+        }
+        if ( Input.GetKey(KeyCode.LeftControl))
+        {
+            foreach (GameObject body in particles) {
+                Destroy(body);
+                
+            }
+            particles.Clear();
+        }
     }
 
 
+    
 
-    private void SpawnParticle()
+
+
+    public void SpawnParticle()
     {
-
+        GameObject test = Instantiate(Prefub,transform.position, UnityEngine.Quaternion.identity);
     }
 }
